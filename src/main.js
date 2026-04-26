317ba9b47069c5535d8e4f1b68bf0aa1dbf2a6c2
Vedant Shendge <179288721+R3curs1on@users.noreply.github.com>
2026-04-26 23:04:46 +0530

    init of boilerplate index.html css main

---
+++ src/main.js
@@ -0,0 +1,25 @@
+
+import "../src/fractals/SierpinskiTriangle" 
+
+$(document).ready(function () {
+    console.log("jQuery loaded");
+});
+
+const canvasDiv = $("canvas")
+const chooseVisDiv = $("choose-visuals")
+const optionSierpinskiTriangle = $("#SierpinskiTriangle")
+const optionSnowFlake = $("#SnowFlake")
+const optionInfiniteCircle = $("#InfiniteCircle")
+
+
+optionInfiniteCircle.ready(function (){
+
+});
+
+optionSierpinskiTriangle.ready(function (){
+
+})
+
+optionSierpinskiTriangle.ready(function() {
+
+})
