eada8ebf85f4397b38ff8cb916d0d2480e4c99ab
Vedant Shendge <179288721+R3curs1on@users.noreply.github.com>
2026-04-29 00:17:16 +0530

    feat : created L system strings and square based geometry (ongoing)

--- src/fractals/simplePattern.js
+++ src/fractals/simplePattern.js
@@ -1,110 +1,78 @@
-// import sleep;
-const recursion = ( str , grammer , depth ) => {
-    if(depth==0) return str ;
-    let nextStr = str.split('').map( currChar => grammer[currChar] || currChar).join('')
-    const res = recursion(nextStr , grammer , depth-1);
-    console.log(res);
-    return res;
+
+/*
+    for pattern like this 
+                 _
+     _         _|_|_
+    |_| ->    |_|_|_|
+                |_|
+    ie expand outermost side with Edge itself ; 
+    we can use xy plane with set<side> vis to update recursively and inserting side so that marks we have drawn a side ;
+    - no memory of grid except set ; 
+    - extra printing function only ;  
+*/
+
+
+const render = ( Edges ) =>{
+    const Xpos  = 0 , Xneg  = 0 , Ypos  = 0  , Yneg  = 0 ;
+    const findMaxXcoord = (Edges) ={
+
+    }
+
+    Xpos = Math.abs(findMaxXcoord) ;
+    Xneg = -Math.abs(findMaxXcoord);
+
+    const findMaxYcoord = (Edges) ={
+
+    }
+
+    Ypos = Math.abs(findMaxYcoord) ;
+    Yneg = -Math.abs(findMaxYcoord);
+    
+    // now grid will be of 2*Xpos * 2*Ypos 
+    // we need to mark  only sides/ segment in set:Edges
+
 }
 
-// const initialStr = `|-_-|\n|-_-|\n|-_-|\n`;
-// const grammer = {
-//     "_" : "|-_-|\n|-_-|\n|-_-|\n",
-//     "|" : "|-_-|\n|---|\n|---|\n|-_-|\n|---|\n|-_-|\n"
-// };
+class Edge{
+    constructor(x,y,dir){
+        this.x=x;
+        this.y=y;
+        this.dir = dir;
+    }
 
-// const initialStr = "▓";
-
-// const grammer = {
-//     "▓" : "▓░",  // Dark becomes dark+light
-//     "░" : "░▓"   // Light becomes light+dark
-// };
-
-// const initialStr = "█";
-
-// const grammer = {
-//     "█" : "█ █",  // A block becomes two blocks with a space
-//     " " : "* █"   // A space becomes three spaces (this keeps the geometry perfectly aligned)
-// }; 
-
-
-
-
-// \u25D3
-// \u25D4
-// \u25D5
-
-
-// const initialStr = "\u25D2";
-
-// const grammer = {
-//     "\u25D2" : "\u25D2 \u25D3",  // A block becomes two blocks with a space
-//     " " : "\u25D2 \u25D3 \u25D4 \u25D5"  , // A space becomes three spaces (this keeps the geometry perfectly aligned)
-//     "\u25D3" : "\u25D3 \u25D4",
-//     "\u25D4" : "\u25D4 \u25D5",
-//     "\u25D5" : "\u25D5 \u25D2"
-// }; 
-
-
-// const initialStr = "\u25DF";
-
-// const grammer = {
-//     "\u25DF" : "\u25DF \u25DE",  // A block becomes two blocks with a space
-//     " " : "\u25DF \u25DE \u25DD \u25DC"  , // A space becomes three spaces (this keeps the geometry perfectly aligned)
-//     "\u25DE" : "\u25DE \u25DD",
-//     "\u25DD" : "\u25DD \u25DC",
-//     "\u25DC" : "\u25DC \u25DF"
-// }; 
-
-
-const initialStr = `_\n| |\n _`;
-
-const grammer = {
-    "_" : "_\n| |\n _" ,
-    "|" : "_\n| |\n _" ,
-}; 
-
-console.log(recursion(initialStr,grammer,0));
-console.log(recursion(initialStr,grammer,1));
-console.log(recursion(initialStr,grammer,2));
-console.log(recursion(initialStr,grammer,6));
-// console.log(recursion(initialStr,grammer,12));
-
-
-
-
-const initialGrid  = [["|","_"],["_","|"]]
-const grammerGrid = {
-    "_" : [["|","_"],["_","|"]],
-    "|" : [["|","_"],["_","|"]]
 }
 
-const expand = (grid, grammar) => {
-    const newGrid = [];
 
-    grid.forEach(row => {
-        let blockRows = [];
+class Square{
+    constructor(bottomLeftX, bottomLeftY){
+        this.x = bottomLeftX;
+        this.y = bottomLeftY;
+    }
+}
 
-        row.forEach(cell => {
-            const block = grammar[cell] ;
 
-            block.forEach((blockRow, i) => {
-                if (!blockRows[i]) blockRows[i] = [];
-                blockRows[i].push(...blockRow);
-            });
-        });
 
-        newGrid.push(...blockRows);
-    });
+function compute (currEdges ) {
 
-    return newGrid;
-};
+}
+const recursion = ( currEdges , depth ) => {
+    if(depth==0) return str ;   
+    const nextSq = compute(Edges);
+    return recursion( nextSq , depth-1 );
+}
+const currSet = new Set();
+currSet.add(Edge(0,0,"top"));
+currSet.add(Edge(0,0,"right"));
+currSet.add(Edge(1,1,"left"));
+currSet.add(Edge(1,1,"down"));
 
-const recursionGrid  = (initialGrid , grammerGrid , depth)=>{
-    if (depth==0 ) return initialGrid;
 
-    const nextGrid = expand(initialGrid,grammerGrid);
-    return recursionGrid(nextGrid,grammerGrid,depth-1);
-}   
+render(currSet);
+
+render( recursion(currSet,1));
+
+
+
+
 
 
